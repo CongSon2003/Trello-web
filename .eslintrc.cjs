4c6c7acebc99f81fabc4cc4fa5ebe9018c34e970
CongSon2003 <sonchuan1@gmail.com>
2024-12-10 20:55:20 +0700

    install Material

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,12 @@
 // Sample Eslint config for React project
 module.exports = {
   env: { browser: true, es2020: true, node: true },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react/jsx-runtime", "plugin:react-hooks/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react", "react-hooks", "react-refresh"],
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import Button from "@mui/material/Button";
+import { AccessAlarm, ThreeDRotation, Delete } from "@mui/icons-material";
 function App() {
-  const [count, setCount] = useState(0);
   return (
-    <>
+    <div>
+      <div>Pham Cong Son</div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
       <div>
-        <h1 style={{ backgroundColor: "red", color: "yellow" }}>Hello world</h1>
+        <Delete color="primary" />
+        <AccessAlarm color="secondary" />
+        <ThreeDRotation color="success" />
       </div>
-    </>
+    </div>
   );
 }
 

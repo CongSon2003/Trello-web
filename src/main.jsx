cdf3679e44afb5eeeb78ea969cb203de49cc5392
CongSon2003 <sonchuan1@gmail.com>
2024-12-10 21:25:01 +0700

    add_CssBaseLine

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import CssBaseline from "@mui/material/CssBaseline"; // Hộ trợ các trình duyệt không bị chạy form css
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>,
-)
+);

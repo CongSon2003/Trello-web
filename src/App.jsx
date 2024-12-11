c9750cc83b3cb66cc4668c9df92222d71e848d31
CongSon2003 <sonchuan1@gmail.com>
2024-12-12 00:00:01 +0700

    config_theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import Button from "@mui/material/Button";
 import { AccessAlarm, ThreeDRotation, Delete } from "@mui/icons-material";
+import Typography from "@mui/material/Typography";
 function App() {
   return (
     <div>
       <div>Pham Cong Son</div>
+      <Typography variant="h1" color="text.secondary">
+        Hello word
+      </Typography>
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
+      <Button variant="contained" color="text.secondary">
+        Success
+      </Button>
+      <Button variant="contained" color="secondary">
+        Contained
+      </Button>
       <Button variant="outlined">Outlined</Button>
       <div>
         <Delete color="primary" />

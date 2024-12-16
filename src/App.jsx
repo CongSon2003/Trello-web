27440e1dafc2461e608ef648ad67689f5ce576e0
CongSon2003 <sonchuan1@gmail.com>
2024-12-16 18:12:20 +0700

    dark_light_p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,56 @@
-import Button from "@mui/material/Button";
-import { AccessAlarm, ThreeDRotation, Delete } from "@mui/icons-material";
-import Typography from "@mui/material/Typography";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  useColorScheme,
+} from "@mui/material";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import NightsStayIcon from "@mui/icons-material/NightsStay";
 function App() {
+  function SelectSmall() {
+    const { mode, setMode } = useColorScheme();
+    const handleChange = (event) => {
+      const selectedMode = event.target.value;
+      console.log(selectedMode);
+      setMode(selectedMode);
+    };
+
+    return (
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+        <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+        <Select
+          labelId="label-select-dark-light-mode"
+          id="select-dark-light-mode"
+          value={mode || ""}
+          label="Mode"
+          onChange={handleChange}
+        >
+          <MenuItem value="light">
+            <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+              <WbSunnyIcon fontSize="small" /> Light
+            </div>
+          </MenuItem>
+          <MenuItem value="system">
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              <SettingsBrightnessIcon fontSize="small" /> System
+            </Box>
+          </MenuItem>
+          <MenuItem value="dark">
+            <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+              <NightsStayIcon fontSize="small" /> Dark
+            </div>
+          </MenuItem>
+        </Select>
+      </FormControl>
+    );
+  }
   return (
     <div>
-      <div>Pham Cong Son</div>
-      <Typography variant="h1" color="text.secondary">
-        Hello word
-      </Typography>
-      <Button variant="text">Text</Button>
-      <Button variant="contained" color="text.secondary">
-        Success
-      </Button>
-      <Button variant="contained" color="secondary">
-        Contained
-      </Button>
-      <Button variant="outlined">Outlined</Button>
-      <div>
-        <Delete color="primary" />
-        <AccessAlarm color="secondary" />
-        <ThreeDRotation color="success" />
-      </div>
+      <h1>Hello world</h1>
+      <SelectSmall />
     </div>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -1,26 +1,20 @@
+import { cyan, deepOrange, orange, teal } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
-
 // Create a theme instance.
 // resource : https://mui.com/material-ui/customization/default-theme/
 const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    // Bộ màu theme custom
-    mode: "light",
-    primary: {
-      main: "#556cd6",
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange,
+      },
     },
-    secondary: {
-      main: "#19857b",
-    },
-    text: {
-      primary: "",
-      disabled: "",
-      secondary: red[500],
-    },
-    error: {
-      main: red.A400,
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange,
+      },
     },
   },
 });
